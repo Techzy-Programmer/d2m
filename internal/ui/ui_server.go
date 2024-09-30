c8a883a9f70307dbc0fbc3a23ded490603e30272
Rishabh Kumar <rishabh.kumar.pro@gmail.com>
2024-09-30 12:26:30 +0530

    Inject API Port dynamically in ui-server

--- internal/ui/ui_server.go
+++ internal/ui/ui_server.go
@@ -5,7 +5,9 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"strings"
 
+	"github.com/Techzy-Programmer/d2m/config/db"
 	"github.com/Techzy-Programmer/d2m/config/paint"
 	"github.com/Techzy-Programmer/d2m/web"
 	"github.com/gin-gonic/gin"
@@ -20,17 +22,23 @@ func embedReact(urlPrefix, buildDirectory string, em embed.FS) gin.HandlerFunc {
 	}
 
 	return func(c *gin.Context) {
+		defer c.Abort()
+
+		if strings.HasSuffix(c.Request.URL.Path, "__API-Port") {
+			c.Data(200, "text/plain", []byte(db.GetConfig[string]("user.APIPort", "8080")))
+			return
+		}
+
 		fileserver.ServeHTTP(c.Writer, c.Request)
-		c.Abort()
 	}
 }
 
 func StartUIServer(port string) {
-	r := gin.Default()
-	r.Use(embedReact("/", "panel/dist", web.EmbeddedFiles))
+	router := gin.Default()
+	router.Use(embedReact("/", "panel/dist", web.EmbeddedFiles))
 
 	paint.Info("Serving ui at :" + port)
-	if err := r.Run(":" + port); err != nil {
+	if err := router.Run(":" + port); err != nil {
 		log.Fatal(err)
 	}
 }
